a439317b7e58237d60c41e25dd3045231f224241
Ngoc Doan <ndoan@soka.edu>
2023-01-19 03:32:10 -0800

    upload other submissions

---
+++ submission/incomplete/hw1.py
@@ -0,0 +1,31 @@
+#Correct Answer
+
+def hw1_1():
+    # Output “Hello, World!” after return
+    return "Hello, World!"
+
+def hw1_2a():
+    # 1 + 10
+    return 1 + 10
+
+def hw1_2b():
+    # 5 - 9
+    return 5 - 9 
+
+def hw1_2c():
+    # 2 / 3
+    return 2 / 3
+
+def hw1_2d():
+    # 9 // 4
+    return 9 // 4
+
+def hw1_2e():
+    pass 
+
+def hw1_2f():
+    pass
+
+def hw1_3():
+    pass
+
