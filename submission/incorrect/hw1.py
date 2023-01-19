81c78def45bceef4dae99b52d0f78cdf765f4844
Ngoc Doan <ndoan@soka.edu>
2023-01-19 03:44:46 -0800

    upload incorrect submission

---
+++ submission/incorrect/hw1.py
@@ -0,0 +1,35 @@
+# Incorrect Answer
+
+def hw1_1():
+    # Output “Hello, World!” after return
+    return Hello, World!
+
+def hw1_2a():
+    # 1 + 10
+    return 1 + 9
+
+def hw1_2b():
+    # 5 - 9
+    return 5 - 9 
+
+def hw1_2c():
+    # 2 / 3
+    return 2 / 3
+
+def hw1_2d():
+    # 9 // 4
+    return 9 % 4
+
+def hw1_2e():
+    # 7**5
+    return 7***5 
+
+def hw1_2f():
+    # 8*2
+    return 8*2
+
+def hw1_3():
+    # Output “Hello, [your name]"
+    myName = "Ngoc"
+    return "Hello," + myName
+
